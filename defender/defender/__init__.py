38be97430b0013580a5fa030f11da98a1e8c9e01
Johnny Le <jwonnyleaf@gmail.com>
2024-04-17 15:31:36 -0500

    Current iteration

--- defender/defender/__init__.py
+++ defender/defender/__init__.py
@@ -3,8 +3,8 @@ import joblib, os, pickle, hashlib, torch, time
 import numpy as np
 from defender.models import MalConvPlus
 import hashlib
-
-from src.extract.feature_extraction import extract_features
+from tensorflow.keras.models import model_from_json
+import ember
 
 LOCAL_FILE_PATH=os.path.join(os.path.dirname(os.path.realpath(__file__)), "../data/sample/002ce0d28ec990aadbbc89df457189de37d8adaadc9c084b78eb7be9a9820c81.exe")
 
@@ -27,46 +27,27 @@ def create_app():
         data = request.get_data()
         print(f"Received {len(data)} bytes of data.")
 
+        # Use Ember to extract features
+        extractor = ember.PEFeatureExtractor()
+        features = extractor.feature_vector(data)
+        features = np.array(features).reshape(1, -1)
+
         predictions = []
 
-        # Extract features from the data
-        features_data, header = extract_features(data)
+        # Load Neural Network Model
+        model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models/model.json")
+        weights_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models/model.h5")
+        with open(model_path, "r") as json_file:
+            model = json_file.read()
 
-        # Load Initial Model
-        clf = joblib.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models/malware_classifier.joblib"))
-        features = pickle.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models/features.pkl"), "rb"))
-        pe_features = np.array([features_data[feature] for feature in features])
-        prediction1 = clf.predict([pe_features])
-        predictions.append(int(prediction1[0]))
+        nn_model = model_from_json(model)
+        nn_model.load_weights(weights_path)
 
-        # Load Malconv Model
-        embed_dim = 8
-        max_len = 4096
-        out_channels = 128
-        window_size = 32
-        dropout = 0.5
-        weight_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models/malconv_plus.pt")
-        if torch.cuda.is_available():
-            device = torch.device("cuda")
-        else:
-            device = torch.device("cpu")
-        model = MalConvPlus(embed_dim, max_len, out_channels, window_size, dropout)
-        model.load_state_dict(torch.load(weight_path))
-        model.to(device)
-        model.eval()
-        input = torch.tensor(header).unsqueeze(0).to(device)
-        prediction2 = model(input)
-        prediction2 = (prediction2 > 0).to(int)
-        predictions.append(int(prediction2[0].item()))
-
-        # Load Bodmas Model
-        bodmas_clf = joblib.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models/bodmas/model.joblib"))
+        nn_prediction = nn_model.predict(features)
+        predictions.append(nn_prediction[0][0])
         
-        prediction3 = bodmas_clf.predict([pe_features])
-        predictions.append(int(prediction3[0]))
-        
-        # Majority Votingß
-        if sum(predictions) >= 2:
+        # Majority Voting
+        if sum(predictions) >= 1:
             malware = 1
         
         end_time = time.time() 
